0f6276fc813bc7934711a83ae7de3d68fe7e7731
Belalcr <130365645+Belalcr@users.noreply.github.com>
2024-01-24 12:02:47 +0300

    Add files via upload

---
+++ pages/Videostar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>catparking</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+          <a href="#" class="link back"><span class=
+          "ios-only">Back</span></a>
+        </div>
+        <div class="title">
+          carparking
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <li class="media-item">
+            <div class="item-content">
+              <div class="item-media"><img src=
+              "./img/carparking.png"
+              class="appicon elevation-5 animated zoomIn"></div>
+              <div class="item-inner">
+                <div class="item-title-row">
+                  <div class="item-title">
+                    carparking
+             </div> <a href="https://t.me/srush_madridi"
+                  class=
+                  "button button-round button-fill button-raised color-green external">کڕین
+                  <i class=
+                  "icon f7-icons color-white">cloud_download</i></a>
+                </div>
+                <div class="item-subtitle">
+                  Hacked
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="block-title">
+         Carparking VIP
+      </div>
+      <div class="block block-strong inset elevation-12">
+        <pre>VIPئەکاونتی </pre>
+        <pre>فوڵ زەمان</pre>
+        <pre>پارەی فوڵ</pre>
+        <pre>سەیارەی جوانی کەم </pre>
+      </div>
+      <div class="list inset elevation-12">
+        <ul>
+          <li>
+
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
